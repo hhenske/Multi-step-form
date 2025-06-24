a92023c2bcebb9e2b859cb0d9881424913584bc9
Holly Henske <hcover333@gmail.com>
2025-06-24 06:59:12 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import StepOne from './components/StepOne';
+
+
+function App() {
+  
+  
+  return (
+    <div className="app">
+      <h1>Multi-Step Form</h1>
+      <StepOne />
+    </div>
+  );
+}
+
+export default App;
